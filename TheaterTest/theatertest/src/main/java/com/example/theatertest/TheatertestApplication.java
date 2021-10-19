4209ab3be483dcaea66184929fd3b53c41f84c30
Nick Gonzalez <nickgonzalez@Nicks-MacBook-Pro.local>
2021-10-19 18:58:57 -0400

    Added example spring webpage

--- TheaterTest/theatertest/src/main/java/com/example/theatertest/TheatertestApplication.java
+++ TheaterTest/theatertest/src/main/java/com/example/theatertest/TheatertestApplication.java
@@ -1,13 +1,35 @@
+// package com.example.theatertest;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class TheatertestApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(TheatertestApplication.class, args);
+// 	}
+
+//}
+
 package com.example.theatertest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TheatertestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TheatertestApplication.class, args);
-	}
+public static void main(String[] args) {
+SpringApplication.run(TheatertestApplication.class, args);
+}
 
+@GetMapping("/hello")
+public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+return String.format("Hello %s!", name);
+}
 }
